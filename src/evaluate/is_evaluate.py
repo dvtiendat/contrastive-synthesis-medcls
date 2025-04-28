1fdecb44871a769aa98b19972f286233b5a32ebb
king17pvp <tuanhai1304@gmail.com>
2025-04-28 22:02:59 +0700

    Added inception score evaluation code

---
+++ src/evaluate/is_evaluate.py
@@ -0,0 +1,129 @@
+import os
+import torch
+import argparse
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torchvision import models, transforms
+from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
+from torchvision.models.inception import inception_v3
+
+from tqdm import tqdm
+from PIL import Image
+from scipy.stats import entropy
+from src.utils.helper import get_path
+from src.models import (
+    acgan,
+    dcgan
+)
+LABEL = ['COVID', 'Lung_Opacity', 'Normal', 'Viral_Pneumonia']
+def inception_score(gen_img_paths, cuda=True, batch_size=32, resize=True, splits=1):
+    """Computes the inception score of the generated images imgs
+
+    imgs -- Torch dataset of (3xHxW) numpy images normalized in the range [-1, 1]
+    cuda -- whether or not to run on GPU
+    batch_size -- batch size for feeding into Inception v3
+    splits -- number of splits
+    """
+    class dataset(torch.utils.data.Dataset):
+        def __init__(self, gen_img_paths = gen_img_paths):
+            self.gen_img_paths = gen_img_paths
+            self.transform = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+            ])
+            self.data = os.listdir(self.gen_img_paths)
+        def __getitem__(self, index):
+            img_path = os.path.join(self.gen_img_paths, self.data[index])
+            img = Image.open(img_path).convert('RGB')  # mở ảnh và chuyển về 3 kênh RGB
+            return self.transform(img)
+
+        def __len__(self):
+            return len(self.data)
+    imgs = dataset(gen_img_paths=gen_img_paths)
+    N = len(imgs)
+    assert batch_size > 0
+    assert N > batch_size
+
+    # Set up dtype
+    if cuda:
+        dtype = torch.cuda.FloatTensor
+    else:
+        if torch.cuda.is_available():
+            print("WARNING: You have a CUDA device, so you should probably set cuda=True")
+        dtype = torch.FloatTensor
+
+    # Set up dataloader
+    dataloader = torch.utils.data.DataLoader(imgs, batch_size=batch_size)
+
+    # Load inception model
+    inception_model = inception_v3(pretrained=True, transform_input=False).type(dtype)
+    inception_model.eval()
+    up = nn.Upsample(size=(299, 299), mode='bilinear').type(dtype)
+    def get_pred(x):
+        if resize:
+            x = up(x)
+        x = inception_model(x)
+        return F.softmax(x, dim=1).data.cpu().numpy()
+
+    # Get predictions
+    preds = np.zeros((N, 1000))
+
+    for i, batch in enumerate(dataloader, 0):
+        batch = batch.type(dtype)
+        batchv = Variable(batch)
+        batch_size_i = batch.size()[0]
+
+        preds[i*batch_size:i*batch_size + batch_size_i] = get_pred(batchv)
+
+    # Now compute the mean kl-div
+    split_scores = []
+
+    for k in range(splits):
+        part = preds[k * (N // splits): (k+1) * (N // splits), :]
+        py = np.mean(part, axis=0)
+        scores = []
+        for i in range(part.shape[0]):
+            pyx = part[i, :]
+            scores.append(entropy(pyx, py))
+        split_scores.append(np.exp(np.mean(scores)))
+
+    return np.mean(split_scores), np.std(split_scores)
+
+# Example usage
+def run_is_evaluate(model_name, path = get_path()):
+    G, D = import_model(model_name)
+    abs_path = get_path()
+    for label in LABEL:
+        gen_img_paths = os.path.join(path, f'images/gen_images/{model_name}/{label}')
+        print(gen_img_paths)
+        if not os.path.exists(gen_img_paths) or len(os.listdir(gen_img_paths)) != 1000:
+            G.sample_images(label, os.path.join(path, 'images/gen_images'), 1000, 25)
+        is_score = inception_score(gen_img_paths, cuda = False)
+        print(f"Inception Score statistics for {model_name} on label {label}: {is_score}")
+def import_model(model_name: str):
+    """
+    Dynamically import model based on model name.
+    """
+    if model_name == "ACGAN":
+        G, D = acgan.get_model({})
+    elif model_name == "DCGAN":
+        G, D = dcgan.get_model({})
+    else:
+        raise ValueError(f"Model {model_name} is not supported.") 
+    return G, D
+def parse_args():
+    parser = argparse.ArgumentParser(description="Compute FID score")
+    parser.add_argument(
+        '--model_name', 
+        type=str, 
+        choices=["ACGAN", "DCGAN"], 
+        default="ACGAN", 
+        help="The model type ('ACGAN' or 'DCGAN'). Default to 'ACGAN'."
+    )
+    return parser.parse_args()
+if __name__ == "__main__":
+    args = parse_args()
+    run_is_evaluate(args.model_name)
