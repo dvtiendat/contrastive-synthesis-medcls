d4e00be878fa5e450f816d85101877368a418d4b
Dat Dao <dvtiendat.work@gmail.com>
2025-05-08 21:17:57 +0700

    cls test init

---
+++ scripts/eval_vit.sh
@@ -0,0 +1,5 @@
+python -m scripts.evaluate_classification \
+    --config configs/classification/finetune_dino.yaml \
+    --checkpoint_path /path/to/your/best_finetune_model.pth \
+    --data_path /path/to/your/data 
+    --output_dir results/test_evaluation 
